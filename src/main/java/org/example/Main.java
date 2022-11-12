a7750259aae71a8aaa2fc4bcb00662d91f61d7e9
Nikita Mykhailov <nikita.mykhailov@adevinta.com>
2022-11-12 12:08:28 +0100

    add SearchUtils; add SortUtils

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+
+    int[] arr = {8, -9, 0, 12, 9};
+    SortUtils sortUtils = new SortUtils();
+    SearchUtils searchUtils = new SearchUtils();
+
+    System.out.println(Arrays.toString(arr));
+
+    System.out.println(Arrays.toString(sortUtils.selectSort(arr)));
+
+    System.out.println(searchUtils.linearSearch(arr, 11));
+
   }
 }
---
+++ src/main/java/org/example/SearchUtils.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SearchUtils {
+
+  public int linearSearch(int arr[], int element){
+    for(int i = 0; i < arr.length; i++){
+      if(arr[i] == element){
+        return i;
+      }
+    }
+    return -1;
+  }
+}
